78cbb5633e7729269f0d5acaee345d62044d1564
ssebadduka <seba.jamir@gmail.com>
2024-04-14 17:45:17 -0700

    addeded allowed host

--- auto_motive_manager/settings.py
+++ auto_motive_manager/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['garage.desta-auto.com','*']
+ALLOWED_HOSTS = ['goritech.org','*']
 
 
 # Application definition
