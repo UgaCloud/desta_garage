e52119474933f8909fa47a76349e27423975c107
ssebadduka <seba.jamir@gmail.com>
2024-04-14 17:28:43 -0700

    addeded mysql db

--- auto_motive_manager/settings.py
+++ auto_motive_manager/settings.py
@@ -101,10 +101,16 @@ WSGI_APPLICATION = 'auto_motive_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mydb',
+		'USER': 'root',
+		'PASSWORD': 'admin',
+		'HOST':'localhost',
+		'PORT':'3306',
     }
 }
+
+
 
 
 # Password validation
